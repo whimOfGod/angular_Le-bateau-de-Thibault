052e0c0b33b7ca6d01728c3448e60c92b5822495
SAMOURA AMADOU <47641181+samoura-amadou@users.noreply.github.com>
2023-10-21 00:15:00 +0200

    Update home-routing.module.ts
    les home-routing

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
+
+
 const routes: Routes = [
   {
     path: '',
